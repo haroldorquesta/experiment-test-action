2d6c8507928e1fe508d5d31d7d69d7a17099dea8
Harold <harold@orq.ai>
2025-05-07 21:24:19 +0800

    test input

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
 import * as core from '@actions/core'
-import { wait } from './wait.js'
+import * as github from "@actions/github";
+
+type Octokit = ReturnType<typeof github.getOctokit>;
+
+type PullRequest = {
+  owner: string;
+  repo: string;
+  issue_number: number;
+};
 
 /**
  * The main function for the action.
@@ -8,15 +16,25 @@ import { wait } from './wait.js'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
     // Log the current timestamp, wait, then log the new timestamp
     core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+
+    const apiKey = core.getInput('api_key')
+
+    if (!apiKey) {
+      throw new Error('Input `api_key` not set!')
+    }
+
+    const path = core.getInput('path')
+
+    if (!path) {
+      throw new Error('Input `path` for yaml configs was not set!')
+    }
+
+    const githubToken = core.getInput("github_token");
+    const octokit = github.getOctokit(githubToken);
+
+    const prs = await inferPullRequestsFromContext(octokit);
 
     // Set outputs for other workflow steps to use
     core.setOutput('time', new Date().toTimeString())
@@ -25,3 +43,34 @@ export async function run(): Promise<void> {
     if (error instanceof Error) core.setFailed(error.message)
   }
 }
+
+const inferPullRequestsFromContext = async (
+  octokit: Octokit,
+): Promise<PullRequest[]> => {
+  const { context } = github;
+  if (Number.isSafeInteger(context.issue.number)) {
+    core.debug(`Use #${context.issue.number} from the current context`);
+    return [
+      {
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        issue_number: context.issue.number,
+      },
+    ];
+  }
+
+  core.debug(`List pull requests associated with sha ${context.sha}`);
+  const pulls = await octokit.rest.repos.listPullRequestsAssociatedWithCommit({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    commit_sha: context.sha,
+  });
+  for (const pull of pulls.data) {
+    core.debug(`  #${pull.number}: ${pull.title}`);
+  }
+  return pulls.data.map(p => ({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    issue_number: p.number,
+  }));
+};
